c33b908dc43ca9ae827bcc301e2f081859db0df8
Abdelrahman Nassar <aanassar.io@gmail.com>
2025-10-16 17:27:14 +0300

    Quartz sync: Oct 16, 2025, 5:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "عبدالرحمن نصار",
+    pageTitleSuffix: "عبدالرحمن نصار",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ar-EG",
+    baseUrl: "https://abdelrahman-ahmed-nassar.github.io/public-obsidian",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
